0370e18e6007b4b59a10d1c50c87cdd190a3b6b6
Matheus Carvalho <matheusvca@gmail.com>
2020-10-28 15:45:14 -0300

    2/2 tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from "react";
+import React,{ useEffect } from "react";
+import { useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [repos,setRepos] = useState([])
+
+  useEffect(()=>{
+    api.get('/repositories').then(response=>{
+      // console.log(response.data);
+      // Aqui a api responde com todos os repositórios, então setamos direto tudo
+      setRepos(response.data);
+
+    })
+  },[]);
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      "url": "https://github.com/Rocketseat/umbriel",
+      "title": "Fastas",
+      "techs": ["Node", "Express", "TypeScript"]
+    })
+      // console.log(response.data)
+      // Aqui a api responde somente o repositório criado, então precisamos resgatar os anteriores
+    setRepos([...repos,response.data])
+
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    const remainedRepos = repos.filter(
+      repo => repo.id !== id
+    );
+
+    setRepos(remainedRepos)
+    
   }
+
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repos.map(repo=>{
+        return (
+
+          <li key={repo.id}>
+          {repo.title}
+          
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+
+        )
+      })}
+
+
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
